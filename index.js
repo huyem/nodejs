6f6e51e9c1e167783dbf8e2a977520beac9c4a18
huyem <64858748+huyem@users.noreply.github.com>
2020-07-16 18:57:09 +0700

    nodejs

--- index.js
+++ index.js
@@ -114,7 +114,7 @@
 // ];
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT||5000
+const PORT = process.env.PORT || 5000;
 //co nghia la neu kog co bieen nao khac thi c=van chajy qua cong 5000
 app.listen(PORT,function () {
     console.log("server is running...");
